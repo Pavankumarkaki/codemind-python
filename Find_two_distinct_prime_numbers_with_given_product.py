17fd4a1df44ddfedee1b292e2f0b9a24c34a8a60
pavankumarkaki <96739003+Pavankumarkaki@users.noreply.github.com>
2022-05-20 20:23:26 +0530

    def isprime(n):     for i in range(2,int(n**0.5)+1):         if n%i==0:             return False     else:         return True n=int(input()) for i in range(2,(n//2)+1):     for j in range(2,(n//2)+1):         if isprime(i) and isprime(j) and i*j==n:             print(i,j)             exit() print(-1)

--- Find_two_distinct_prime_numbers_with_given_product.py
+++ Find_two_distinct_prime_numbers_with_given_product.py
@@ -1,17 +1,13 @@
-def ip(m):
-     if m==1:
-          return False
-     else:
-          for i in range(2,int(m**0.5)+1):
-               if m%i==0:
-                    return False
-          else:
-               return True
+def isprime(n):
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+    else:
+        return True
 n=int(input())
-for i in range(2,n+1):
-    for j in range(2,n+1):
-        if ip(i) and ip(j) and i*j==n:
+for i in range(2,(n//2)+1):
+    for j in range(2,(n//2)+1):
+        if isprime(i) and isprime(j) and i*j==n:
             print(i,j)
             exit()
-else:
-    print(-1)
+print(-1)
