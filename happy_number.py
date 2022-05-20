23f31fcffe28aba01fa1e888035205b6d46c2f7d
pavankumarkaki <96739003+Pavankumarkaki@users.noreply.github.com>
2022-05-20 20:31:50 +0530

    def hap(n):     if n<10:         return n
        s=0
        while n>0:
            s+=(n%10)**2
            n//=10
        return hap(s)
            
    n=int(input())
    if hap(n)==1 or hap(n)==7:
        print(True)
    else:
        print(False)

--- happy_number.py
+++ happy_number.py
@@ -1,16 +1,15 @@
 def hap(n):
+    if n<10:
+        return n
+    
     s=0
     while n>0:
-        r=n%10
-        s+=r**2
+        s+=(n%10)**2
         n//=10
-    return s
+    return hap(s)
+        
 n=int(input())
-while hap(n)>9:
-    n=hap(n)
 if hap(n)==1 or hap(n)==7:
     print(True)
 else:
     print(False)
-    
-    
