806d4b4bc311a10da882e598313ca939859120a6
pavankumarkaki <96739003+Pavankumarkaki@users.noreply.github.com>
2022-05-20 20:41:46 +0530

    def isprime(n):     if n==1:         return False     else:         for i in range(2,int(n**0.5)+1):             if n%i==0:                 return False         return True n=int(input()) m=int(input()) i=1 while not isprime(i+n+m):     i+=1 print(i)

--- Battle_of_Primes.py
+++ Battle_of_Primes.py
@@ -1,16 +1,14 @@
-def isprime(m):
-     if m==1:
-          return False
-     else:
-          for i in range(2,int(m**0.5)+1):
-               if m%i==0:
-                    return False
-          else:
-               return True
+def isprime(n):
+    if n==1:
+        return False
+    else:
+        for i in range(2,int(n**0.5)+1):
+            if n%i==0:
+                return False
+        return True
 n=int(input())
 m=int(input())
-t=n+m
 i=1
-while not isprime(t+i):
+while not isprime(i+n+m):
     i+=1
 print(i)
