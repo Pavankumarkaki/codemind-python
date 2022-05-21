14944819e4490c4e7c1104ce1313aa544b4e1822
pavankumarkaki <96739003+Pavankumarkaki@users.noreply.github.com>
2022-05-22 01:14:56 +0530

    def isprime(n):     if n==1:         return False     else:         for i in range(2,int(n**0.5)+1):             if n%i==0:                 return False         return True n=int(input()) l=list(map(int,input().split())) c=0 for i in l:     if isprime(i):         c+=1 print(c)

---
+++ Primes_in_the_array.py
@@ -0,0 +1,15 @@
+def isprime(n):
+    if n==1:
+        return False
+    else:
+        for i in range(2,int(n**0.5)+1):
+            if n%i==0:
+                return False
+        return True
+n=int(input())
+l=list(map(int,input().split()))
+c=0
+for i in l:
+    if isprime(i):
+        c+=1
+print(c)
