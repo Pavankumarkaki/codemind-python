f42b9100e6754ca4e7caa40bc8fd447a06e115fe
pavankumarkaki <96739003+Pavankumarkaki@users.noreply.github.com>
2022-06-03 21:39:51 +0530

    s=input() s=s.lower() k=list(set(s)) k.sort() for i in k:     if i==" ":         del k[k.index(i)] print("".join(k))

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,8 +1,8 @@
 s=input()
 s=s.lower()
-g=[]
-for i in s:
-    if s.count(i)==1 and i!=" ":
-        g.append(i)
-g.sort()
-print("".join(g))
+k=list(set(s))
+k.sort()
+for i in k:
+    if i==" ":
+        del k[k.index(i)]
+print("".join(k))
