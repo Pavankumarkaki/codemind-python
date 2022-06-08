62aeb7f177fee401561b95b6924700d7f472ed5f
pavankumarkaki <96739003+Pavankumarkaki@users.noreply.github.com>
2022-06-08 12:44:13 +0530

    n=int(input()) a="ABCDEFGHIJKLMNOPQRSTUVWXYZ" for i in range(n):     for j in range(n):         print(a[i],end=" ")     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,8 +1,6 @@
 n=int(input())
-for i in range(1,n+1):
-    for  j in range(1,n+1):
-        if i==j or j==(n+1)-i:
-            print("x",end="")
-        else:
-            print("0",end="")
+a="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+for i in range(n):
+    for j in range(n):
+        print(a[i],end=" ")
     print()
