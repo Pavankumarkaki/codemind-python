239fcec8abe7cee4f38a43d2e093c1f70fe6db31
pavankumarkaki <96739003+Pavankumarkaki@users.noreply.github.com>
2022-06-27 22:19:11 +0530

    def revs(n):     rev=0     while n>0:         r=n%10         rev=(rev*10)+r         n//=10     return rev n=int(input()) if n>0:     print(revs(n)) else:     print(-1*revs(abs(n)))

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -1,12 +1,12 @@
+def revs(n):
+    rev=0
+    while n>0:
+        r=n%10
+        rev=(rev*10)+r
+        n//=10
+    return rev
 n=int(input())
-rev=0
-nn=n
-n=abs(n)
-while n>0:
-    r=n%10
-    rev=rev*10+r
-    n//=10
-if nn>0:
-    print(rev)
+if n>0:
+    print(revs(n))
 else:
-    print(-1*rev)
+    print(-1*revs(abs(n)))
