585df7dd4552ea53ced48f6e2effada3057b8ccc
pavankumarkaki <96739003+Pavankumarkaki@users.noreply.github.com>
2022-06-27 21:21:14 +0530

    def add(n):     s=0     while n>0:         r=n%10         s+=r         n=n//10     return s n=int(input()) while(n>10):     n=add(n) print(n)

--- Add_Digits.py
+++ Add_Digits.py
@@ -1,9 +1,11 @@
-a=int(input())
-while a>9:
-    su=0
-    while a>0:
-        r=a%10;
-        su+=r
-        a=a//10
-    a=su
-print(a)
+def add(n):
+    s=0
+    while n>0:
+        r=n%10
+        s+=r
+        n=n//10
+    return s
+n=int(input())
+while(n>10):
+    n=add(n)
+print(n)
