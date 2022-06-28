721cae865ed44effb776b29d8f6a3c2ab956dee6
pavankumarkaki <96739003+Pavankumarkaki@users.noreply.github.com>
2022-06-28 14:15:55 +0530

    n=int(input()) s=0 for i in range(1,n):     if n%i==0:         s+=i if s>n:     print(True) else:     print(False)

--- Abundant_Numbers.py
+++ Abundant_Numbers.py
@@ -1,11 +1,9 @@
-def spf(n):
-    s=0
-    for i in range(1,n):
-        if n%i==0:
-            s+=i
-    return s
 n=int(input())
-if spf(n)>n:
+s=0
+for i in range(1,n):
+    if n%i==0:
+        s+=i
+if s>n:
     print(True)
 else:
     print(False)
