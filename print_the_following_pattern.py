cd13ed03dda4b89ea826eba28668e6931c6e7ce0
pavankumarkaki <96739003+Pavankumarkaki@users.noreply.github.com>
2022-11-05 17:52:00 +0530

    n=int(input()) alpha="@ABCDEFGHIJKLMNOPQRSTUVWXYZ" for i in range(n,0,-1):     for j in range(i):         print(alpha[i],end=" ")     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,6 +1,6 @@
 n=int(input())
-a="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-for i in range(n):
-    for j in range(n):
-        print(a[i],end=" ")
+alpha="@ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+for i in range(n,0,-1):
+    for j in range(i):
+        print(alpha[i],end=" ")
     print()
